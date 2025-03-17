091639e1c5a21c3cfa74f26e5735a86520be6a13
LAB53 <lab53@fatec.com>
2025-03-17 10:49:25 -0300

    salva as strings 'palavraSecreta e letra' com os dados inseridos pelo usuário

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string palavraSecreta;
+string letra;
+
+Console.Clear();
+
+Console.Write("Digite qual será a palavra secreta: ");
+palavraSecreta = Console.ReadLine()!;
+
+Console.Clear();
+
+Console.Write("Digite uma letra: ");
+letra = Console.ReadLine()!;
