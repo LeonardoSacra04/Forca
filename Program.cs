f3f2be60cb33561c02a4a1a0cc7a682db4d326e3
GuilhermeCerabando <cerabando@gmail.com>
2025-03-24 10:13:38 -0300

    Analisa se a letra dada pelo usuário existe na palavra secreta

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿string palavraSecreta;
 string letra;
+bool letraExiste;
 
 Console.Clear();
+Console.WriteLine("-----Forca-----");
 
-Console.Write("Digite qual será a palavra secreta: ");
+Console.WriteLine("Escreva qual será a palavra secreta: ");
 palavraSecreta = Console.ReadLine()!;
 
 Console.Clear();
+Console.WriteLine("-----Forca-----");
 
-Console.Write("Digite uma letra: ");
-letra = Console.ReadLine()!;
+Console.WriteLine("Escreva uma letra: ");
+letra = Console.ReadLine()!
+        .Trim()
+        .Substring(0, 1)
+        .ToLower();
+
+
+Console.Clear();
+Console.WriteLine("-----Forca-----");
+
+letraExiste = palavraSecreta
+            .ToLower()
+            .Contains(letra);
+
+Console.WriteLine($"A letra {letra} existe na palavra secreta? {letraExiste}");
